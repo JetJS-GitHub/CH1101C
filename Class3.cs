f5aff0193181f15eff8d512f2c5bd6911a0ae40b
Jet Sun <18820095105@139.com>
2018-04-09 18:56:35 +0800

    增加了第三个类

---
+++ Class3.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CH1101C
+{
+    class Class3
+    {
+        long i = 0;
+        //增加第三个类
+    }
+}
