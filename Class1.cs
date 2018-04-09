be75282e17aabb724e4462e79021b8828224fd20
Jet Sun <18820095105@139.com>
2018-04-09 18:36:51 +0800

    Example for GitHub

---
+++ Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CH1101C
+{
+    class Class1
+    {
+        private int i = 0;
+
+        private bool j = false; //Example for GitHub
+    }
+}
