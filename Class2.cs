0c6e50de6a57975820ec7d5c305e1139c1ff49ec
Jet Sun <18820095105@139.com>
2018-04-09 18:45:13 +0800

    Add Class2

---
+++ Class2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CH1101C
+{
+    class Class2
+    {
+
+        // This is Class2.
+        internal Int32 i = 0;
+    }
+}
